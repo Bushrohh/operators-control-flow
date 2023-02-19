cb1e2da3f41a4ca9bcc9304dc037bc0cb9e20188
bushrohh <sekerebushroh@gmail.com>
2023-02-19 13:42:18 +0100

    operators controls

---
+++ index.js
@@ -0,0 +1,32 @@
+// question 3 for bolatito
+let scienceSubjects = 'physics, chemistry, biology, technical drawing'
+let socialSubjects = 'accounting, commerce, marketing, geography'
+let artSubjects = 'government, economics, literature, history'
+let generalSubjects = 'english, mathematics'
+let dept = major
+
+if 
+   (dept = artSubjects){
+        console.log("here are the subjects for arts:" + artSubjects + "" +generalSubjects)
+    }
+    else{
+        console.log(generalSubjects)
+    }
+
+    // question 4
+    for (let i = 1; i < 20; i += 7){
+        console.log(i);
+    }
+    answer = 8;
+
+    // question 5 power and number
+    let number = 20;
+    let power = 16;
+    let offer = 'not correct'
+    
+    if (number=20, power= 16){
+        console.log("The number" +"" + power + "is the power of 2 nearest to" + "" +number)
+    }
+    else{
+        console.log(offer)
+    }
---
+++ readme.md
@@ -0,0 +1,41 @@
+<!-- list the symbols for each of the below javascript operators -->
+1) Arithmetic Operators:
+*, -, +, / 
+examples are; let x= 4;
+              let y= 2;
+
+     console.log(x+y);
+     console.log(y*x);
+
+2)Assignment Operators:
+*=, /=, +=,-=,
+examples are; let a = 7;
+              let b = 8;
+
+         console.log(a+=b);
+         console.log(b-=a);
+             
+
+3)Comparison Operators:
+==, !=, ===
+examples are; let b = 4;
+              let z = 10;
+               let c = 4;
+
+      console.log(b != z);
+      console.log(b == c);
+
+4) Logical Operators:
+&&, !!, !
+examples are; let H = bushroh;
+              let Y = sekere;
+
+     conole.log(H && Y);
+     console.log(H !! Y);    
+
+5) Bitwise Operators:
+^, ~, !, <<
+examples are; let z = 10;
+              let x = 2;
+
+    console.log(z^x);                       
